bb3bb715cff7211d502bc7699ccd059b16ba93de
pkulik0 <pkulik0@icloud.com>
2022-05-30 01:18:04 +0200

    Added an initial design of the welcome view

--- noetsi/ContentView.swift
+++ noetsi/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    
+    @State private var accentColors: [Color] = [.red, .blue, .pink, .cyan, .green, .yellow]
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack(spacing: 0) {
+                Text("Welcome to ")
+                    .font(.title.bold())
+                Text("noetsi")
+                    .font(.title.bold())
+                    .foregroundColor(accentColors.randomElement()!)
+                    .padding(10)
+                    .background(RoundedRectangle(cornerRadius: 10).fill())
+            }
+            
+                
+            VStack {
+                TextField("Username", text: $username)
+                    .font(.headline)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).fill().opacity(0.3))
+                    .padding(.bottom)
+
+                TextField("Password", text: $password)
+                    .font(.headline)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).fill().opacity(0.3))
+                    .onSubmit {
+                        username = "k"
+                    }
+                    .padding(.bottom, 25)
+                
+                Button("Continue") { }
+                    .font(.title.bold())
+                    .foregroundColor(accentColors.randomElement()!)
+                    .padding(10)
+                    .background(RoundedRectangle(cornerRadius: 10).fill())
+            }
             .padding()
+        }
+        
     }
 }
 
