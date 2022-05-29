966bd926e9446ba3d86271efe4889424869eeeb7
pkulik0 <pkulik0@icloud.com>
2022-05-29 22:31:32 +0200

    Initial Commit

---
+++ noetsi/noetsiApp.swift
@@ -0,0 +1,17 @@
+//
+//  noetsiApp.swift
+//  noetsi
+//
+//  Created by qurrie on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct noetsiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
