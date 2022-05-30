df29e86f55d7403ff0bed65e0930ed63051cd87c
pkulik0 <pkulik0@icloud.com>
2022-05-30 14:06:25 +0200

    Add FirestoreManager class & Note struct

---
+++ noetsi/FirestoreManager.swift
@@ -0,0 +1,45 @@
+//
+//  FirestoreManager.swift
+//  noetsi
+//
+//  Created by qurrie on 30/05/2022.
+//
+
+import Firebase
+
+class FirestoreManager: ObservableObject {
+    var notes: [Note] = []
+
+    var uid: String {
+        if let user = Auth.auth().currentUser {
+            return String(user.uid)
+        }
+        return "-1"
+    }
+    
+    init() {
+        fetchNotes()
+    }
+    
+    func fetchNotes() {
+        let db = Firestore.firestore()
+        
+        db.collection(uid).getDocuments { (querySnapshot, error) in
+            if let error = error {
+                // TODO: handle better?
+                print(error.localizedDescription)
+                return
+            }
+            for document in querySnapshot!.documents {
+                var note: Note = Note(id: document.documentID, title: "", body: "", tags: [])
+                
+                note.title = document.data()["title"] as? String ?? "Unknown title"
+                note.body = document.data()["body"] as? String ?? "Unknown content"
+                note.tags = document.data()["tags"] as? [String] ?? []
+                
+                self.notes.append(note)
+                print("\(note)")
+            }
+        }
+    }
+}
---
+++ noetsi/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  noetsi
+//
+//  Created by qurrie on 30/05/2022.
+//
+
+import Foundation
+
+struct Note {
+    let id: String
+    var title: String
+    var body: String
+    var tags: [String]
+}
