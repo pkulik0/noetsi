9e3d01afdfdca8c7e2faa37107cc7506b7ffd661
pkulik0 <pkulik0@icloud.com>
2022-06-16 23:10:53 +0200

    Move DragToHide to a separate file

---
+++ noetsi/DragToHide.swift
@@ -0,0 +1,42 @@
+//
+//  DragToHide.swift
+//  noetsi
+//
+//  Created by qurrie on 16/06/2022.
+//
+
+import SwiftUI
+
+struct DragToHide: ViewModifier {
+    @Binding var offset: CGSize
+    @Binding var isPresented: Bool
+
+    func body(content: Content) -> some View {
+        content
+            .offset(offset)
+            .gesture(DragGesture(minimumDistance: 10, coordinateSpace: .global)
+                .onChanged({ value in
+                    let amountVertical = value.translation.height
+                    if amountVertical > 0 {
+                        withAnimation {
+                            offset.height = amountVertical
+                        }
+                    }
+                })
+                .onEnded({ value in
+                    withAnimation {
+                        if value.translation.height > 40 {
+                            isPresented = false
+                        }
+                        offset = CGSize.zero
+                    }
+                })
+            )
+    }
+}
+
+extension View {
+    func hideOnDrag(offset: Binding<CGSize>, isPresented: Binding<Bool>) -> some View {
+        modifier(DragToHide(offset: offset, isPresented: isPresented))
+    }
+}
--- noetsi/HorizontalPickerView.swift
+++ noetsi/HorizontalPickerView.swift
@@ -47,37 +47,3 @@ struct HorizontalPickerView<ItemType: Hashable, Content: View>: View {
         .clipShape(Capsule())
     }
 }
-
-extension View {
-    func hideOnDrag(offset: Binding<CGSize>, isPresented: Binding<Bool>) -> some View {
-        modifier(DragToHide(offset: offset, isPresented: isPresented))
-    }
-}
-
-struct DragToHide: ViewModifier {
-    @Binding var offset: CGSize
-    @Binding var isPresented: Bool
-
-    func body(content: Content) -> some View {
-        content
-            .offset(offset)
-            .gesture(DragGesture(minimumDistance: 10, coordinateSpace: .global)
-                .onChanged({ value in
-                    let amountVertical = value.translation.height
-                    if amountVertical > 0 {
-                        withAnimation {
-                            offset.height = amountVertical
-                        }
-                    }
-                })
-                .onEnded({ value in
-                    withAnimation {
-                        if value.translation.height > 40 {
-                            isPresented = false
-                        }
-                        offset = CGSize.zero
-                    }
-                })
-            )
-    }
-}
